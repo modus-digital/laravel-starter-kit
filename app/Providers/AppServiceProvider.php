1b1d288cb8f3aabf290db900325f73d4aa42c855
Alex van Steenhoven <alex@modus-digital.com>
2025-05-01 23:58:12 +0200

    feat(providers): enhance AppServiceProvider with model safety features and add FolioServiceProvider for authentication page routing

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,34 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Vite;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
-    public function register(): void
-    {
-        //
-    }
+    public function register(): void {}
 
     /**
      * Bootstrap any application services.
      */
     public function boot(): void
     {
-        //
+        // Dangerous commands are prohibited in production
+        DB::prohibitDestructiveCommands(prohibit: app()->isProduction());
+
+        // Models
+        Model::unguard(); // Disable model guards
+        Model::shouldBeStrict(); // Throw exceptions when accessing non-existent properties
+        Model::preventLazyLoading(); // Throw exceptions when lazy loading is used
+        Model::automaticallyEagerLoadRelationships(); // Resolve n+1 issues
+
+        // Auto HTTPS Scheme + Vite prefetch strategy
+        URL::forceScheme('https');
+        Vite::usePrefetchStrategy('aggressive');
     }
 }
---
+++ app/Providers/FolioServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Folio\Folio;
+
+class FolioServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void {}
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        // Authentication Pages (for middleware reasons separated from application pages)
+        Folio::path(resource_path('views/pages/authentication'))
+            ->uri('/auth')
+            ->middleware(['*' => []]);
+
+        // Application Pages
+        Folio::path(resource_path('views/pages/application'))
+            ->middleware(['*' => ['auth', 'verified']]);
+    }
+}
