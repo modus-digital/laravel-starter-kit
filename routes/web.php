47082ae4a2aa6af526759453fed7057d02fb37f5
Alex van Steenhoven <alex@modus-digital.com>
2025-05-02 00:02:02 +0200

    feat(routes): update web routes to include authentication redirects and middleware for enhanced user session management

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Livewire\Volt\Volt;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\EmailVerificationController;
+use App\Http\Controllers\Auth\ClearBrowserSessionsController;
 
-Volt::route('/dashboard', 'dashboard');
+Route::redirect('/', route('login'));
+
+// Auth routes
+Route::redirect('/login', 'auth/login')->name('login');
+Route::redirect('/register', 'auth/register')->name('register');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/email/verify/{id}/{hash}', EmailVerificationController::class)->middleware(['signed', 'throttle:6,1'])->name('verification.verify');
+    Route::delete('auth/clear-sessions', ClearBrowserSessionsController::class)->name('auth.clear-sessions');
+    Route::post('/auth/logout', LogoutController::class)->name('auth.logout');
+});
