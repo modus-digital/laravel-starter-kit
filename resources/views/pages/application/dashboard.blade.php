dd4d251b433e62e700fe6e1c4dccf80a1e8cf0df
Alex van Steenhoven <alex@modus-digital.com>
2025-05-04 23:49:56 +0200

    feat(views): update dashboard greeting to display authenticated user's name for personalized experience

--- resources/views/pages/application/dashboard.blade.php
+++ resources/views/pages/application/dashboard.blade.php
@@ -4,11 +4,12 @@ use function Laravel\Folio\name;
 
 name('application.dashboard');
 
+
 ?>
 
 <x-layouts.app title="Dashboard">
   <h1 class="text-3xl font-bold dark:text-white font-clash-display">
-    Welkom Thim
+    Welkom {{  auth()->user()->name }}
   </h1>
   <p class="text-xl mt-1 text-neutral-500 max-w-lg leading-6">
     Dit is het Modus standaard dashboard. Begin nu met het bouwen of aanpassen van je nieuwe applicatie!
