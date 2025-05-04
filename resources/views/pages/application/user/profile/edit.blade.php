809daceb88cb243ad3c8e2306e9ec072e27b3fa6
Alex van Steenhoven <alex@modus-digital.com>
2025-05-05 01:13:53 +0200

    feat(composer, views): add wire-elements/modal dependency and create user profile edit and settings pages - Added "wire-elements/modal" to composer.json for modal component support. - Created user profile edit and settings Blade views for enhanced user profile management.

---
+++ resources/views/pages/application/user/profile/edit.blade.php
@@ -0,0 +1,12 @@
+<?php
+
+use function Laravel\Folio\name;
+
+name('user.profile.edit');
+
+?>
+
+<x-layouts.app title="profile">
+    <h1>profile edit</h1>
+</x-layouts.app>
+
---
+++ resources/views/pages/application/user/profile/settings.blade.php
@@ -0,0 +1,12 @@
+<?php
+
+use function Laravel\Folio\name;
+
+name('user.profile.settings');
+
+?>
+
+<x-layouts.app title="profile settings">
+    <h1>profile settings</h1>
+</x-layouts.app>
+
