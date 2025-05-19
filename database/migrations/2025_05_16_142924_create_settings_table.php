acc63b05c65c015253eda447ecdcd97fa28abacb
Alex van Steenhoven <alex@modus-digital.com>
2025-05-19 10:57:39 +0200

    refactor(migrations): Consolidate user settings and application settings migrations; create new mail-related tables
    - Added user_settings table to the initial users migration.
    - Removed obsolete application_settings migration.
    - Introduced new migrations for mail-related tables, including mails, attachments, events, and mailables.
    - Updated settings migration to insert default values for application settings.

--- database/migrations/2025_05_16_142924_create_settings_table.php
+++ database/migrations/2025_05_16_142924_create_settings_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use Carbon\Carbon;
+use Illuminate\Container\Attributes\DB;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -8,15 +10,23 @@ return new class extends Migration
 {
     public function up(): void
     {
+        // Create the settings table
         Schema::create(config('settings.database_table_name'), function (Blueprint $table) {
             $table->id();
-            $table->string('key')
-                ->unique()
-                ->index();
-            $table->json('value')
-                ->nullable();
+            $table->string('key')->unique()->index();
+            $table->json('value')->nullable();
             $table->timestamps();
         });
+
+        // Insert the default settings
+        DB::table(config('settings.database_table_name'))->insert([
+            ['key' => 'general.app_name', 'value' => 'Laravel', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['key' => 'general.logo', 'value' => null, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['key' => 'features.auth.register', 'value' => true, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['key' => 'features.auth.login', 'value' => true, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['key' => 'features.auth.password_reset', 'value' => true, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['key' => 'features.auth.email_verification', 'value' => true, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+        ]);
     }
 
     public function down(): void
