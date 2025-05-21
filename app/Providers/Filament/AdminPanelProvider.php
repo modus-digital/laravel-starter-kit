266ebfdedd61db73394df7522bb667830ed3c32d
Alex van Steenhoven <alex@modus-digital.com>
2025-05-21 15:55:37 +0200

    chore: Refactor AdminServiceProvider

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -6,16 +6,13 @@ use App\Filament\Pages\BackupsPage;
 use App\Filament\Pages\HealthChecksResultsPage;
 use App\Filament\Pages\Settings;
 use App\Http\Middleware\Filament\ApplyUserTheme;
-use App\Http\Middleware\Filament\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Navigation\NavigationGroup;
-use Filament\Navigation\NavigationItem;
 use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
-use Filament\Support\Colors\Color;
 use Filament\View\PanelsRenderHook;
 use Filament\Widgets\AccountWidget;
 use Filament\Widgets\FilamentInfoWidget;
@@ -37,21 +34,20 @@ class AdminPanelProvider extends PanelProvider
 {
     public function panel(Panel $panel): Panel
     {
-        $applicationName = setting('general.app_name');
-
         return $panel
             ->default()
             ->id('admin')
-            ->darkMode(false, false)
             ->path('admin')
-            ->brandName($applicationName)
-            ->colors(['primary' => Color::Blue])
+            ->brandName(name: setting('general.app_name'))
             ->routes(fn() => FilamentMails::routes())
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([Dashboard::class])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
-            ->widgets([AccountWidget::class, FilamentInfoWidget::class])
+            ->widgets([
+                AccountWidget::class,
+                FilamentInfoWidget::class,
+            ])
             ->middleware([
                 EncryptCookies::class,
                 AddQueuedCookiesToResponse::class,
@@ -67,9 +63,9 @@ class AdminPanelProvider extends PanelProvider
             ->plugins([
                 FilamentSpatieLaravelHealthPlugin::make()->usingPage(HealthChecksResultsPage::class),
                 FilamentSpatieLaravelBackupPlugin::make()->usingPage(BackupsPage::class),
+                FilamentSettingsPlugin::make()->pages([Settings::class]),
                 TranslationManagerPlugin::make(),
                 FilamentMailsPlugin::make(),
-                FilamentSettingsPlugin::make()->pages([Settings::class])
             ])
             ->navigationGroups([
                 NavigationGroup::make()->label(__('admin.navigation.groups.beheer')),
