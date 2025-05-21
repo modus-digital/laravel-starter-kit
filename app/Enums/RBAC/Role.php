4b15eccd1766119928e21f3c5caba2d3f447c9b4
Alex van Steenhoven <alex@modus-digital.com>
2025-05-21 14:44:09 +0200

    feat: Add toArray method to Role enum for easier role management
    - Introduced a static toArray method that returns an associative array of role values and their display names.
    - Cleaned up comments in the Role enum for better readability.
    - Enhanced the color method with improved formatting for maintainability.

--- app/Enums/RBAC/Role.php
+++ app/Enums/RBAC/Role.php
@@ -8,12 +8,12 @@ namespace App\Enums\RBAC;
  */
 enum Role: string
 {
-    /**
+/**
      * The super admin role.
      */
     case SUPER_ADMIN = 'Super-administrator';
 
-    /**
+/**
      * The default role for users.
      */
     case USER = 'Gebruiker';
@@ -45,12 +45,17 @@ enum Role: string
 
     public function color(): string
     {
-        // bg-blue-50 text-blue-700 ring-blue-700/10 dark:bg-blue-900 dark:text-blue-300 dark:ring-blue-300/20
-        // bg-rose-100 text-rose-800 ring-rose-800/10 dark:bg-rose-900 dark:text-rose-300 dark:ring-rose-300/20
-        //
         return match ($this) {
             self::SUPER_ADMIN => 'bg-rose-100 text-rose-800 ring-rose-800/10 dark:bg-rose-900 dark:text-rose-300 dark:ring-rose-300/20',
             self::USER => 'bg-sky-100 text-sky-800 ring-sky-800/10 dark:bg-sky-900 dark:text-sky-300 dark:ring-sky-300/20'
         };
+    }
+
+    public static function toArray(): array
+    {
+        return array_combine(
+            array_column(self::cases(), 'value'),
+            array_map(fn(self $role) => $role->displayName(), self::cases())
+        );
     }
 }
