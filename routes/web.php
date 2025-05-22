929d2c9f44292b179d8abaf0ce6ff489cd19f06a
Alex van Steenhoven <alex@modus-digital.com>
2025-05-22 16:49:05 +0200

    feat: Implement StopImpersonating functionality for user session management
    - Added StopImpersonating controller to handle user impersonation termination.
    - Created a new migration for telescope_entries table to support monitoring.
    - Updated web routes to include a POST route for leaving impersonation.
    - Enhanced navigation header with a button to stop impersonation, improving user experience.
    - Added language support for the "Stop impersonation" feature in the UI.

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@
 use App\Http\Controllers\Auth\EmailVerificationController;
 use App\Http\Controllers\Auth\LogoutController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StopImpersonating;
 
 Route::redirect('/', route('login'));
 
@@ -15,4 +16,6 @@ Route::redirect('/register', 'auth/register')->name('register');
 Route::middleware('auth')->group(function (): void {
     Route::get('/email/verify/{id}/{hash}', EmailVerificationController::class)->middleware(['signed', 'throttle:6,1'])->name('verification.verify');
     Route::post('/auth/logout', LogoutController::class)->name('auth.logout');
+
+    Route::post('/impersonate/leave', StopImpersonating::class)->name('impersonate.leave');
 });
