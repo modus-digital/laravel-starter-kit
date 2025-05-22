d70e76afc2f28b2b4c74c2df007391e7bad2fca1
Alex van Steenhoven <alex@modus-digital.com>
2025-05-22 16:48:17 +0200

    feat: Add Telescope integration for enhanced application monitoring
    - Introduced TelescopeServiceProvider to manage Telescope's configuration and request filtering.
    - Added configuration files for Telescope, including telescope.php and telescope-toolbar.php, to customize its behavior and appearance.
    - Included necessary assets such as app.js, app.css, and app-dark.css for Telescope's frontend.
    - Updated providers list to register TelescopeServiceProvider, enabling its functionality within the application.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,8 +2,9 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\FolioServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\FolioServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
     App\Providers\VoltServiceProvider::class,
     Jenssegers\Agent\AgentServiceProvider::class,
 ];
