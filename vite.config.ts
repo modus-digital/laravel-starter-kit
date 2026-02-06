d2a0a5ef8f7d6025c494be61818dd492ceca3cfa
Alex van Steenhoven <alex@modus-digital.com>
2026-02-06 14:14:54 +0100

    fix: Removed Filament from vite

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.tsx', 'resources/css/filament/control/theme.css'],
+            input: ['resources/css/app.css', 'resources/js/app.tsx'],
             ssr: 'resources/js/ssr.tsx',
             refresh: true,
         }),
