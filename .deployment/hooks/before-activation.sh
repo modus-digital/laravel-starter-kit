30c1192a4df23523862135aca80d356c10791fed
Alex van Steenhoven <alex@modus-digital.com>
2026-02-10 15:20:21 +0100

    chore(deployment): Remove unnecessary Filament optimization commands and update API documentation generation

--- .deployment/hooks/before-activation.sh
+++ .deployment/hooks/before-activation.sh
@@ -2,7 +2,6 @@ php_executable=$1
 
 # Clearing old caches
 "$php_executable" artisan optimize:clear
-"$php_executable" artisan filament:optimize-clear
 
 # Creating a symlink to the storage directory.
 "$php_executable" artisan storage:link
@@ -13,14 +12,8 @@ php_executable=$1
 "$php_executable" artisan route:cache
 "$php_executable" artisan view:cache
 
-# Filament optimization
-"$php_executable" artisan filament:optimize
-
 # Running migrations
 "$php_executable" artisan migrate --force
 
-# Syncing changelogs
-"$php_executable" artisan changelog:sync
-
-# Regenerate API Docs
-"$php_executable" artisan l5-swagger:generate
+# Generating api documentation
+"$php_executable" artisan scribe:generate
